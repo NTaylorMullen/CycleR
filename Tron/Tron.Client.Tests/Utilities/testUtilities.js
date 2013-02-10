e921d3b91df3e5baa46766acdd46ce507af82b4b
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-09 20:50:35 -0800

    Added a testUtilities class that has a "theory" method that can be used in unit tests.

---
+++ Tron/Tron.Client.Tests/Utilities/testUtilities.js
@@ -0,0 +1,11 @@
+﻿var testUtilities;
+
+(function ($, window) {
+    testUtilities = {
+        theory: function (argumentList, fn) {
+            for (var i = 0; i < argumentList.length; i++) {
+                fn.apply(this, argumentList[i]);
+            }
+        }
+    };
+})($, window)
