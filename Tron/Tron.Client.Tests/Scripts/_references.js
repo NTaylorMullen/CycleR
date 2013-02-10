11d91e8c261f05e40cac9c6b50a015291cbca15d
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-09 20:49:19 -0800

    Added some required libraries to the client.js.tests folder

---
+++ Tron/Tron.Client.Tests/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="qunit.js" />
+/// <reference path="three.detector.js" />
+/// <reference path="three.fpscontrols.js" />
+/// <reference path="three.min.js" />
+/// <reference path="three.trackballcontrols.js" />
+/// <reference path="three.windowresize.js" />
+/// <reference path="../Utilities/testUtilities.js" />
